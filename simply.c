0ec424377c7d115966789b2332945bb877ee33f5
Yangfan0401 <2958193707@qq.com>
2023-11-26 11:36:13 +0800

    Hello Document
    Simply hello

---
+++ simply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//Function:
+
+/*
+* Author:YangFan
+* File Path: /Untitled-1
+* Date: Create in 2023-11-26 11:35:31
+*/
+int main(){
+  printf("Hello World!");
+
+    return 0;
+}
